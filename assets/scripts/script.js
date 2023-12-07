032032a07db703585c58ee87be6e54fecd4ed10d
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-12-07 16:56:26 +0300

    adjust theme-switcher

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,49 @@
 let year = document.querySelector(".year");
 year.textContent = "Copyright © " + new Date().getFullYear();
+
+const STORAGEKEY = "theme-preference";
+
+const handleThemeSwitch = () => {
+  THEME.value = THEME.value === "light" ? "dark" : "light";
+  setThemeColor();
+};
+
+const getThemeColor = () => {
+  if (localStorage.getItem(STORAGEKEY)) return localStorage.getItem(STORAGEKEY);
+  else
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+};
+
+const THEME = {
+  value: getThemeColor(),
+};
+
+const setThemeColor = () => {
+  localStorage.setItem(STORAGEKEY, THEME.value);
+  displayThemeColor();
+};
+
+const displayThemeColor = () => {
+  document.firstElementChild.setAttribute("data-theme", THEME.value);
+  document
+    .querySelector("#theme-toggle")
+    ?.setAttribute("aria-label", THEME.value);
+};
+
+displayThemeColor();
+
+window.onload = () => {
+  displayThemeColor();
+  document
+    .querySelector("#theme-toggle")
+    .addEventListener("click", handleThemeSwitch);
+};
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    THEME.value = isDark ? "dark" : "light";
+    setThemeColor();
+  });
